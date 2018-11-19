7a5ff095618b29193729bb57ac01541ba87dcab9
Alexandr <alexandr.kizilow@gmail.com>
2018-11-19 10:37:23 +0300

    delete request /status

--- main.js
+++ main.js
@@ -4,36 +4,45 @@ require('dotenv').config()
 
 const app = expess()
 
-app.get('/', (req, res) => {
+const url = 'https://api.cloudpayments.ru/orders/create'
+const urlStatus = 'https://api.cloudpayments.ru/payments/find'
+const urlHook = 'https://app.botmother.com/api/bot/action/H1bjU9lDm/FzD5BZMDpDCCSwZCqCBC-JxDzBvzBJCBcukBWKDJD0BVCcpDnBnD1BgB_PBaWqDs' 
+
+const responseForBot = {
+    platform: 'tg',
+    users: 'everyone',
+    data: {
+        message: ''
+    }
+}
+
+app.get('/', async (req, res) => {
 
     const cost = req.query.cost
     const count = req.query.count
     const total = cost * count
-    const url = 'https://api.cloudpayments.ru/orders/create'
+    
 
     if(total){
 
         const body = {
             'Amount':total,
             'Currency':'RUB',
-            'Description':'Оплата на сайте example.com',
-            'Email':'client@test.local',
-            'RequireConfirmation':true,
-            'SendEmail':false
+            'Description':'Оплата в боте',
         }
-        fetch(url, {
+        const response = await fetch(url, {
             method: 'post',
             body: JSON.stringify(body),
             headers: {'Content-Type': 'application/json',
                 'Authorization': 'Basic ' + Buffer.from(`${process.env.USER_PAY}:${process.env.PASS}`).toString('base64')
             }
         })
-            .then(res => res.json())
-            .then(json => res.send(json))
+        const resp = await response.json()
+        console.log(resp)
     } else{
         res.send({'err':'no total'})
     }
-        
+  
 })
 
 app.listen(8080, () => {
