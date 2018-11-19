a8168bf5e146a098661135e1ef00b7544d4410ab
Alexandr <alexandr.kizilow@gmail.com>
2018-11-19 21:57:09 +0300

    add body for subscription request

---
+++ subscription.js
@@ -0,0 +1,14 @@
+const subscription = {
+    token: '',
+    accountId:'user@example.com',
+    description:'Ежемесячная подписка на сервис',
+    email:'user@example.com',
+    amount: '100',
+    currency:'RUB',
+    requireConfirmation:false,
+    startDate:'',
+    interval:'',
+    period:1
+}
+
+module.exports = subscription
